3d9e1fe96a36b4b4ad5226a52ef47440680070c1
linrongjian <linrongjian@wulifun.com>
2023-05-04 10:53:52 +0800

    fix

--- component/logserver/logserver.go
+++ component/logserver/logserver.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package logserver
 
 import (
@@ -61,67 +60,3 @@ func (s *logServer) Stop() error {
 func (s *logServer) Options() Options {
 	return s.opts
 }
-=======
-package logserver
-
-import (
-	"eventgo/component/logserver/modules/journal"
-	"eventgo/core/app"
-	"eventgo/core/network/protocols/mqwrap"
-)
-
-var (
-	logReport *journal.LogReport
-)
-
-type LogServer interface {
-	app.Server
-	Init(...Option) error
-	Options() Options
-}
-
-type Option func(*Options)
-
-type logServer struct {
-	*app.App
-	opts Options
-}
-
-func NewLogServer(opts ...Option) LogServer {
-	options := Options{}
-	for _, o := range opts {
-		o(&options)
-	}
-	app := app.NewApp()
-	app.Init()
-	return &logServer{
-		App:  app,
-		opts: options,
-	}
-}
-
-func (s *logServer) Init(opts ...Option) error {
-	for _, o := range opts {
-		o(&s.opts)
-	}
-	s.AddFlags(mqwrap.Flags)
-	s.App.InitComplete()
-	return nil
-}
-
-func (s *logServer) Run() error {
-	mqwrap.Startup()
-	logReport = journal.NewLogReport()
-	logReport.Init()
-	s.App.Run()
-	return nil
-}
-
-func (s *logServer) Stop() error {
-	return nil
-}
-
-func (s *logServer) Options() Options {
-	return s.opts
-}
->>>>>>> da5a225f254f88f123db39a87f8aeaac593c5682
