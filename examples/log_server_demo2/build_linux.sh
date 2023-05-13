5e4b41b067041e87504b50557d7eb68dcaad2a42
linrongjian <linrongjian@wulifun.com>
2023-05-13 11:27:04 +0800

    fix

--- examples/log_server_demo2/build_linux.sh
+++
@@ -1,8 +0,0 @@
-CURRENT_DIR=$(cd $(dirname $0); pwd)
-cd $CURRENT_DIR
-
-GOOS=linux CGO_ENABLED=0 GOARCH=amd64 \
-go build \
--o log_server_demo2_linux \
--tags "release" \
--ldflags "-X main.Developer=zl" .
